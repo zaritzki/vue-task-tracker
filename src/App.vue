71846e3602bb5d2995e39b62e5aee6e8b5cbb78e
zaritzki <zars195@gmail.com>
2021-10-24 18:15:42 +0100

    fetchdata from fake api port 5000 - using json-server

--- src/App.vue
+++ src/App.vue
@@ -51,28 +51,21 @@ export default {
         this.tasks = this.tasks.filter((task) => task.id !== id)
       }
     },
+    async fetchTasks() {
+      const res = await fetch('http://localhost:5000/tasks')
+      const data = await res.json()
+
+      return data
+    },
+    async fetchTask(id) {
+      const res = await fetch(`http://localhost:5000/tasks/${id}`)
+      const data = await res.json()
+
+      return data
+    },
   },
-  created() {
-    this.tasks = [
-      {
-        id: 1,
-        title: 'Doctors Appointment',
-        day: 'March 1st at 2:30pm',
-        reminder: true,
-      },
-      {
-        id: 2,
-        title: 'Meeting at School',
-        day: 'March 3rd at 1:30pm',
-        reminder: true,
-      },
-      {
-        id: 3,
-        title: 'Food Shopping',
-        day: 'March 3rd at 11:00am',
-        reminder: false,
-      },
-    ]
+  async created() {
+    this.tasks = await this.fetchTasks()
   },
 }
 </script>
